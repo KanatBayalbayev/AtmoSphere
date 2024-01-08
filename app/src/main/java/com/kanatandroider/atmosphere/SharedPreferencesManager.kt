01abb4f0bd7cad20fd71d33c6859328d27b97ee0
Kanat Bayalbayev <Kanatkz07@mail.ru>
2024-01-08 14:22:27 +0600

    impl base

---
+++ app/src/main/java/com/kanatandroider/atmosphere/SharedPreferencesManager.kt
@@ -0,0 +1,23 @@
+package com.kanatandroider.atmosphere
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferencesManager(context: Context) {
+    private val prefs = context.getSharedPreferences(
+        "com.kanatandroider.atmosphere.PREFS",
+        Context.MODE_PRIVATE
+    )
+
+    fun saveString(key: String, value: String) {
+        with(prefs.edit()) {
+            putString(key, value)
+            apply()
+        }
+    }
+
+    fun getString(key: String, defaultValue: String): String? {
+        return prefs.getString(key, defaultValue)
+    }
+
+}
