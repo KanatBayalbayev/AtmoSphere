855e6a6587d51608758f65cbcedf05a3878a0f48
Kanat Bayalbayev <Kanatkz07@mail.ru>
2024-01-25 16:40:40 +0600

    added terms and confi

---
+++ app/src/main/java/com/kanatandroider/atmosphere/presentation/activities/TermsOfUseActivity.kt
@@ -0,0 +1,12 @@
+package com.kanatandroider.atmosphere.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kanatandroider.atmosphere.R
+
+class TermsOfUseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_of_use)
+    }
+}
